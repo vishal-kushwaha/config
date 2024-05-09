8497befc9777b6a89290d7db230252473d682468
Vishal Singh Kushwaha <vishal.02jan@gmail.com>
2024-05-09 20:20:42 +0530

    Gruvbox dark theme

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -8,7 +8,7 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
 -- vim options
-vim.opt.background = "light"
+vim.opt.background = "dark"
 vim.opt.cmdheight = 0
 vim.opt.foldenable = false
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
