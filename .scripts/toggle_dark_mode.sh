3bde33548bb51356e46a6af7ea190305923da32b
Vishal Singh Kushwaha <vishal.02jan@gmail.com>
2024-06-24 14:01:04 +0530

    Adds catppucchin theme

--- .scripts/toggle_dark_mode.sh
+++ .scripts/toggle_dark_mode.sh
@@ -5,16 +5,15 @@ MODE=$(/opt/homebrew/bin/dark-notify -e)
 ALACRITTY_CONFIG_PATH="/Users/vkushwaha/.config/alacritty/alacritty.toml"
 TMUX_CONFIG_PATH="/Users/vkushwaha/.config/tmux/tmux.conf"
 LAZYGIT_CONFIG_PATH="/Users/vkushwaha/.config/lazygit/config.yml"
-LAZYNVIM_PATH="/Users/vkushwaha/.local/share/nvim/lazy"
 
 if [[ $MODE == "dark" ]]; then
-	sed -i "" "s/_day/_night/g" "$ALACRITTY_CONFIG_PATH"
-	sed -i "" "s/day/night/g" "$TMUX_CONFIG_PATH"
-	ln -sf "$LAZYNVIM_PATH/tokyonight.nvim/extras/lazygit/tokyonight_night.yml" "$LAZYGIT_CONFIG_PATH"
+	sed -i "" "s/_latte/_mocha/g" "$ALACRITTY_CONFIG_PATH"
+	sed -i "" "s/latte/mocha/g" "$TMUX_CONFIG_PATH"
+	ln -sf "$LAZYGIT_CONFIG_PATH/../themes/lazygit/themes/mocha/rosewater.yml" "$LAZYGIT_CONFIG_PATH"
 else
-	sed -i "" "s/_night/_day/g" "$ALACRITTY_CONFIG_PATH"
-	sed -i "" "s/night/day/g" "$TMUX_CONFIG_PATH"
-	ln -sf "$LAZYNVIM_PATH/tokyonight.nvim/extras/lazygit/tokyonight_day.yml" "$LAZYGIT_CONFIG_PATH"
+	sed -i "" "s/_mocha/_latte/g" "$ALACRITTY_CONFIG_PATH"
+	sed -i "" "s/mocha/latte/g" "$TMUX_CONFIG_PATH"
+	ln -sf "$LAZYGIT_CONFIG_PATH/../themes/lazygit/themes/latte/rosewater.yml" "$LAZYGIT_CONFIG_PATH"
 fi
 
 # reload tmux
