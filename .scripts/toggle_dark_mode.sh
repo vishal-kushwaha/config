c294f2c939615622197463bfb3dac0a0cd2c1336
Vishal Singh Kushwaha <vishal.02jan@gmail.com>
2025-03-02 23:29:12 +0530

    cleanup

--- .scripts/toggle_dark_mode.sh
+++ .scripts/toggle_dark_mode.sh
@@ -2,16 +2,16 @@
 
 MODE=$(/opt/homebrew/bin/dark-notify -e)
 
-ALACRITTY_CONFIG_PATH="/Users/vkushwaha/.config/alacritty/alacritty.toml"
+# ALACRITTY_CONFIG_PATH="/Users/vkushwaha/.config/alacritty/alacritty.toml"
 TMUX_CONFIG_PATH="/Users/vkushwaha/.config/tmux/tmux.conf"
 LAZYGIT_CONFIG_PATH="/Users/vkushwaha/.config/lazygit/config.yml"
 
 if [[ $MODE == "dark" ]]; then
-  sed -i "" "s/_latte/_mocha/g" "$ALACRITTY_CONFIG_PATH"
+  # sed -i "" "s/_latte/_mocha/g" "$ALACRITTY_CONFIG_PATH"
   sed -i "" "s/latte/mocha/g" "$TMUX_CONFIG_PATH"
   cp -f "$LAZYGIT_CONFIG_PATH/../themes/lazygit/themes/mocha/rosewater.yml" "$LAZYGIT_CONFIG_PATH"
 else
-  sed -i "" "s/_mocha/_latte/g" "$ALACRITTY_CONFIG_PATH"
+  # sed -i "" "s/_mocha/_latte/g" "$ALACRITTY_CONFIG_PATH"
   sed -i "" "s/mocha/latte/g" "$TMUX_CONFIG_PATH"
   cp -f "$LAZYGIT_CONFIG_PATH/../themes/lazygit/themes/latte/rosewater.yml" "$LAZYGIT_CONFIG_PATH"
 fi
