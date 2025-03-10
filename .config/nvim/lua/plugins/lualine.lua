b63a3bd08a529bc6a2f32a2de9d7fccd165b1611
Vishal Singh Kushwaha <vishal.02jan@gmail.com>
2025-03-10 14:47:54 +0530

    fix lualine separators to match tmux rounded theme

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-lualine/lualine.nvim",
   opts = {
+    options = {
+      section_separators = { left = "", right = "" },
+    },
     sections = {
       lualine_y = { "location" },
       lualine_z = { "progress" },
