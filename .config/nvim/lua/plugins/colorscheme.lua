d973de981ddeb99e68abc2e67663b1b9312a6d1e
Vishal Singh Kushwaha <vishal.02jan@gmail.com>
2025-03-11 12:03:00 +0530

    cleanup tmux config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -23,11 +23,9 @@ return {
       update_interval = 1000,
       set_dark_mode = function()
         vim.o.background = "dark"
-        -- vim.cmd("colorscheme gruvbox")
       end,
       set_light_mode = function()
         vim.o.background = "light"
-        -- vim.cmd("colorscheme gruvbox")
       end,
     },
   },
